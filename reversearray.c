082c9fe63b3b99c153aefde8201f9e714eed6daa
Mustafa1765 <shaikmustafaali1765@gmail.com>
2025-09-28 13:56:29 +0530

    Create reversearray.c

---
+++ reversearray.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main (){
+	int n,i;
+	printf("Enter no of elements: ");
+	scanf("%d",&n);
+
+	printf("Enter elements: ");
+	for(i=0;i<n;i++){
+		scanf("%d",&a[i]);
+	}
+	printf("The reversed array: ");
+	
+	}
+}
