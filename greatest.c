fc2f5919d7d4880a7be9cec584d17468114313f5
Mustafa1765 <shaikmustafaali1765@gmail.com>
2025-09-28 13:13:11 +0530

    greatest.c
    Search course
    Type to search
    Minimum 3 characters required for search
    Close
    4.1.2. Greatest of three numbers using a conditional operator
    14:23
    
    
    
    
    
    Write a C program to display the greatest of three numbers using a conditional operator (ternary operator).
    
    
    
    Input Format:
    
    The first three lines of input each contain one integer representing 
    n
    u
    m
    1
    , 
    n
    u
    m
    2
    , and 
    n
    u
    m
    3
    , respectively.
    
    
    Output Format:
    
    The output displays the greatest of the three entered integers.
    
    
    Note: Refer to sample test cases for better understanding.

---
+++ greatest.c
@@ -0,0 +1,14 @@
+// Type Content here...
+#include<stdio.h>
+int main(){
+	int num1,num2,num3;
+	printf("num1: ");
+	scanf("%d",&num1);
+	printf("num2: ");
+	scanf("%d",&num2);
+	printf("num3: ");
+	scanf("%d",&num3);
+	
+	
+	return 0;
+}
