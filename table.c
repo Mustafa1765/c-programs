424fb59dce8f11966a76d52e3552edeb6be7339a
Mustafa1765 <shaikmustafaali1765@gmail.com>
2025-09-28 11:52:47 +0530

    Create table.c

---
+++ table.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+ int i,n;
+ printf("input the num (table to be calculated): ");
+ scanf("%d",&n);
+ if()
+ {
+    for(i=0;i<=10;i++)
+    {
+        printf("%d*%d=%d\n",n,);
+    }
+
+    else
+    {
+        printf("invalid input");
+    }  
+
+  
+ return 
+}
