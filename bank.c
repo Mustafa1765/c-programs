896488ba2a252711d34e370a5e85b6bfea56a4dc
Mustafa1765 <shaikmustafaali1765@gmail.com>
2025-09-28 11:49:25 +0530

    Create bank.c

---
+++ bank.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+ int  currentbalance = 1000;
+void great()
+{
+    printf("Welcome to SBI Bank .......!");
+
+}
+int deposit(int amount)
+{
+    currentbalance = ;
+}
+int credit(int amount)
+{
+    currentbalance = currentbalance - amount;
+
+}
+int returnbalance(int amount)
+{
+return currentbalance;
+}
+ void close()
+{
+printf("Thanks for using  SBI...! \n visit again.**");
+
+{
+    great();
+    //currentbalance();
+printf("\nThe basic amount u have is : %d\n",currentbalance);
+deposit(500);
+printf("now the balance is : %d\n",currentbalance);
+
+printf("now the balance is : %d\n",currentbalance);
+returnbalance(currentbalance);
+printf("now the balance is : %d\n",currentbalance);
+close();
+}
