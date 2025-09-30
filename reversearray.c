ac953aed321f87a1906387f018efe66c60437f83
Manir <maniruz.zaman.primary@gmail.com>
2025-10-01 03:01:13 +0530

    Update reversearray.c

--- reversearray.c
+++ reversearray.c
@@ -1,14 +1,19 @@
-#include<stdio.h>
-int main (){
-	int n,i;
-	printf("Enter no of elements: ");
-	scanf("%d",&n);
+#include <stdio.h>
+int main() {
+    int n, i;
+    printf("Enter no of elements: ");
+    scanf("%d", &n);
 
-	printf("Enter elements: ");
-	for(i=0;i<n;i++){
-		scanf("%d",&a[i]);
-	}
-	printf("The reversed array: ");
-	
-	}
+    int a[n]; 
+    printf("Enter elements: ");
+    for(i = 0; i < n; i++){
+        scanf("%d", &a[i]);
+    }
+
+    printf("The reversed array: ");
+    for(i = n-1; i >= 0; i--){
+        printf("%d ", a[i]);
+    }
+    printf("\n");
+    return 0;
 }
